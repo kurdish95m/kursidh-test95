930a2e90308c7affdab69ddbf17cfcf7feddd7ae
kurdish95m <osama95.tech@gmail.com>
2022-10-13 00:07:10 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# kursidh-test95" 
---
+++ github.bat
@@ -0,0 +1,9 @@
+echo "# kursidh-test95" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kurdish95m/kursidh-test95.git
+git push -u origin main
+pause
+
